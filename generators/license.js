8f34d2af7afeee2c5eec75df6a39c92f3ac23661
Brian Cardarella <bcardarella@gmail.com>
2013-02-08 22:53:14 -0500

    Initial commit

---
+++ generators/license.js
@@ -0,0 +1,5 @@
+// ==========================================================================
+// Project:   Ember FormBuilder
+// Copyright: ©2013 DockYar, LLC. and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
