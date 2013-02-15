83cca3868112f38526a00992090b32db3bcfc799
Brian Cardarella <bcardarella@gmail.com>
2013-02-15 13:43:19 -0500

    Prevent default

--- packages/ember-formBuilder/lib/views/form.js
+++ packages/ember-formBuilder/lib/views/form.js
@@ -1,8 +1,13 @@
 Ember.FormBuilder.Form = Ember.View.extend({
   tagName: 'form',
+  attributeBindings: ['novalidate'],
+  novalidate: 'novalidate',
   submit: function(event) {
     if (this.get('context').validate === undefined || this.get('context').validate()) {
       this.get('controller').send('submit');
+    }
+    if (event) {
+      event.preventDefault();
     }
   }
 });
