5485d7c6105f1912bfbe0d72ad54b3e25ad67379
Antoine Catton <acatton@fusionbox.com>
2013-10-11 17:30:07 -0600

    Remove warnings about bindings
    After updating ember.js ember-easyForm was generating warnings like:
    
    “Warning: You're attempting to render a view by passing
    propertyBinding=view.property to a view helper, but this syntax is
    ambiguous. You should either surround view.property in quotes or remove
    `Binding` from propertyBinding.”

--- packages/ember-easyForm/lib/templates/wrappedInput.js
+++ packages/ember-easyForm/lib/templates/wrappedInput.js
@@ -1,1 +1,1 @@
-Ember.TEMPLATES['easyForm/wrapped_input'] = Ember.Handlebars.compile('{{label-field propertyBinding=view.property textBinding=view.label}}<div class="{{unbound view.controlsWrapperClass}}">{{partial "easyForm/inputControls"}}</div>');
+Ember.TEMPLATES['easyForm/wrapped_input'] = Ember.Handlebars.compile('{{label-field propertyBinding="view.property" textBinding="view.label"}}<div class="{{unbound view.controlsWrapperClass}}">{{partial "easyForm/inputControls"}}</div>');
