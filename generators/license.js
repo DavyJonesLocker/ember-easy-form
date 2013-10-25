57c07e0d9d496b17698e9ebb101c50e168a28452
Robert Jackson <robert.w.jackson@me.com>
2013-10-24 21:44:01 -0400

    Fix typo in license.js.

--- generators/license.js
+++ generators/license.js
@@ -1,5 +1,5 @@
 // ==========================================================================
 // Project:   Ember EasyForm
-// Copyright: Copyright 2013 DockYar, LLC. and contributors.
+// Copyright: Copyright 2013 DockYard, LLC. and contributors.
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
