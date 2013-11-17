ac9b96f70d97bf946cab22a57ec253512cad991c
Bradley Priest <bradleypriest@gmail.com>
2013-11-17 14:27:57 +0800

    Remove unused utility method

--- packages/ember-easyForm/lib/utilities.js
+++ packages/ember-easyForm/lib/utilities.js
@@ -1,8 +1,3 @@
-Ember.EasyForm.objectNameFor = function(object) {
-  var constructorArray = object.constructor.toString().split('.');
-  return constructorArray[constructorArray.length - 1].underscore();
-};
-
 Ember.EasyForm.processOptions = function(property, options) {
   if (options) {
     options.hash.property = property;
