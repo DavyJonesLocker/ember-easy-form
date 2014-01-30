48afb96dbea276b67a2080de773a48417b86bb3e
Manuel Wiedenmann <manuel@funkensturm.de>
2014-01-30 23:32:15 +0100

    adds i18n support discussed in #99

--- packages/ember-easyForm/tests/utilities_test.js
+++ packages/ember-easyForm/tests/utilities_test.js
@@ -3,5 +3,33 @@ module('EasyForm utility methods', {
 });
 
 test('humanizes string', function() {
-  equal(Ember.EasyForm.humanize("firstName"), 'First name');
+  equal(Ember.EasyForm.humanize('firstName'), 'First name');
+});
+
+test('humanizes key string', function() {
+  equal(Ember.EasyForm.humanize('users.first_name'), 'Users.first name');
+});
+
+test('mutation of options - only property', function() {
+  equal(Ember.EasyForm.processOptions('firstName'), 'firstName');
+});
+
+test('mutation of options - property and options', function() {
+  var options = {hash: {placeholder: 'First name'}};
+  deepEqual(Ember.EasyForm.processOptions('firstName', options), {hash: {placeholder: 'First name', property: 'firstName'}});
+});
+
+test('mutation of options - property and *Translation options without Ember.I18n', function() {
+  var options = {hash: {placeholderTranslation: 'users.first_name'}};
+  deepEqual(Ember.EasyForm.processOptions('firstName', options), {hash: {placeholderTranslation: 'users.first_name', property: 'firstName'}});
+});
+
+test('mutation of options - property and *Translation options with Ember.I18n', function() {
+  Ember.I18n = {
+    t: function(key) {
+      return Ember.EasyForm.humanize(key);
+    }
+  };
+  var options = {hash: {placeholderTranslation: 'users.first_name'}};
+  deepEqual(Ember.EasyForm.processOptions('firstName', options), {hash: {placeholder: 'Users.first name', property: 'firstName'}});
 });
