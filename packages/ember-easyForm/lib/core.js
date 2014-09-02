0413d92bf94e898f9150cf096bf5499ab03b3f2e
Arthur Gunn <arthur@gunn.co.nz>
2014-09-03 09:33:43 +1200

    Register version with ember.

--- packages/ember-easyForm/lib/core.js
+++ packages/ember-easyForm/lib/core.js
@@ -1,3 +1,7 @@
 Ember.EasyForm = Ember.Namespace.create({
   VERSION: 'VERSION_STRING_PLACEHOLDER'
 });
+
+if (Ember.libraries) {
+  Ember.libraries.register("Ember EasyForm", Ember.EasyForm.VERSION);
+}
