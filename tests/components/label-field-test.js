4a80ef136407182d2a0b3f0e9dd7ccbbbe9e1beb
Diogo Mafra <eudega@gmail.com>
2015-11-01 17:23:57 -0200

    Enable some tests of the label-field helper

--- tests/components/label-field-test.js
+++ tests/components/label-field-test.js
@@ -1,6 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-// import config from 'ember-easy-form/config';
+import config from 'ember-easy-form/config';
 
 moduleForComponent('label-field', 'Integration | Component | label field', {
   integration: true
@@ -15,28 +15,16 @@ test('renders a label field with custom text', function(assert) {
   this.render(hbs`{{label-field firstName text="Your first name"}}`);
   assert.equal(this.$().find('label').text(), 'Your first name');
 });
-//
-// test('uses the wrapper config', function() {
-//   Ember.EasyForm.Config.registerWrapper('my_wrapper', {labelClass: 'my-label'});
-//   view = Ember.View.create({
-//     template: templateFor('{{#form-for controller wrapper="my_wrapper"}}{{label-field firstName}}{{/form-for}}'),
-//     container: container,
-//     controller: controller
-//   });
-//   append(view);
-//   ok(view.$().find('label.my-label').get(0), 'labelClass not defined');
-// });
-//
-// test('uses the defined template name', function() {
-//   Ember.TEMPLATES['custom-label-template'] = templateFor('My custom label | {{view.labelText}}');
-//   Ember.EasyForm.Config.registerWrapper('my_wrapper', {labelTemplate: 'custom-label-template'});
-//
-//   view = Ember.View.create({
-//     template: templateFor('{{#form-for controller wrapper="my_wrapper"}}{{label-field firstName}}{{/form-for}}'),
-//     container: container,
-//     controller: controller
-//   });
-//   append(view);
-//   equal(view.$().text(), "My custom label | First name");
-// });
-//
+
+test('uses the wrapper config', function(assert) {
+  config.registerWrapper('my_wrapper', {labelClass: 'my-label'});
+  this.render(hbs`{{#form-for controller wrapper="my_wrapper"}}{{label-field firstName}}{{/form-for}}`);
+  assert.ok(this.$().find('label.my-label').get(0), 'labelClass not defined');
+});
+
+test('uses the defined template name', function(assert) {
+  this.container.register('template:custom-label-template', hbs`My custom label | {{view.labelText}}`);
+  config.registerWrapper('my_wrapper', {labelTemplate: 'custom-label-template'});
+  this.render(hbs`{{#form-for controller wrapper="my_wrapper"}}{{label-field firstName}}{{/form-for}}`);
+  assert.equal(this.$().text(), "My custom label | First name");
+});
