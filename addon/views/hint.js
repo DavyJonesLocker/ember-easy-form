251e2f55f817a23441cc1f4343a8fcfb9b087596
Diogo Mafra <eudega@gmail.com>
2015-11-01 16:41:01 -0200

    Move the hint-field helper to Ember-cli

--- addon/views/hint.js
+++ addon/views/hint.js
@@ -1,4 +1,7 @@
-Ember.EasyForm.Hint = Ember.EasyForm.BaseView.extend({
+import Ember from 'ember';
+import BaseView from 'ember-easy-form/views/base';
+
+export default BaseView.extend({
   tagName: 'span',
   classNameBindings: ['wrapperConfig.hintClass'],
   templateName: Ember.computed.oneWay('wrapperConfig.hintTemplate'),
